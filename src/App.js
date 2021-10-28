029ffc987859cf2c8cab8db9f1693131c4ecac5d
uhliko <ondrej.uhlik@vodafone.com>
2021-10-28 11:58:12 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+import { CardList } from './components/card-list/card-list.component.jsx'
+import { SearchBox } from './components/search-box/search-box.component.jsx'
 
-function App() {
+export const App = () => {
+  const [monsters, setMonsters] = useState();
+  const [filteredMonsters, setFilteredMonsters] = useState([]);
+  const [searchField, setSearchField] = useState("");
+
+  axios.get("https://jsonplaceholder.typicode.com/users").then(response => setMonsters(response.data));
+
+  useEffect(() => {
+    if (monsters) {
+      setFilteredMonsters(monsters.filter(monster => {
+        return monster.name.toLowerCase().includes(searchField.toLowerCase())
+      }))
+    }
+  }, [monsters, searchField])
+  
+
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Monsters Rolodex</h1>
+    <SearchBox placeholder="search monsters" handleChange={(e) => setSearchField(e.target.value)}/>
+      {filteredMonsters && (
+        <CardList monsters={filteredMonsters} search={searchField}>
+    
+        </CardList>
+      )}
     </div>
+    </>
   );
 }
-
-export default App;
